02ae49d972271ec3bce31615424e6378cfd400d5
FIDIMALALA Micael Jerry <fidimalala.mj@gmail.com>
2025-01-21 21:55:07 +0300

    fix: update put error func

--- src/error/put_error.c
+++ src/error/put_error.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 05:45:30 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/21 05:51:26 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/21 21:53:59 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	put_error(char *msg)
 {
+	ft_putendl_fd("Error", 2);
 	ft_putendl_fd(msg, 2);
 	exit(1);
 }
