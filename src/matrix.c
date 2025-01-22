f8112bb06b5190747f573e68439754d291d7ea75
FIDIMALALA Micael Jerry <fidimalala.mj@gmail.com>
2025-01-22 11:56:27 +0300

    fix: free all memory allocated

--- src/matrix.c
+++ src/matrix.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 18:23:54 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/21 21:32:44 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/22 11:54:51 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,20 @@ char	**matrix_char_join(char **matrix, char *new)
 	char	*end_line;
 
 	i = 0;
-	res = (char **)malloc((ft_matrixsize((void **)matrix) + 1) * sizeof(char *)
-			+ 1);
+	res = (char **)malloc((ft_matrixsize((void **)matrix) + 2) * sizeof(char *));
 	if (!res)
 		return (NULL);
 	if (matrix)
 	{
 		while (matrix[i])
 		{
-			res[i] = matrix[i];
+			res[i] = ft_strdup(matrix[i]);
 			end_line = ft_strchr(res[i], '\n');
 			if (end_line)
 				*end_line = '\0';
 			i++;
 		}
+		ft_matrixfree((void **)matrix);
 	}
 	res[i] = new;
 	i++;
