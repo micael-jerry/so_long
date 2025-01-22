0a3496730f219b256c6f9e5738dfc3f0878cc72d
FIDIMALALA Micael Jerry <fidimalala.mj@gmail.com>
2025-01-22 10:54:30 +0300

    fix: check pointer on str len func

--- lib/ft_printf/libft/ft_strlen.c
+++ lib/ft_printf/libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:31:53 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/23 10:48:19 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/22 10:43:28 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
+	if (!str)
+		return (0);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
