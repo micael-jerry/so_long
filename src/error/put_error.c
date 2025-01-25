97575fda0370c0b745132b55c67147bf0c88432f
FIDIMALALA Micael Jerry <fidimalala.mj@gmail.com>
2025-01-25 10:51:00 +0300

    fix: check err message

--- src/error/put_error.c
+++ src/error/put_error.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 05:45:30 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/21 21:53:59 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/25 10:49:44 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	put_error(char *msg)
 {
 	ft_putendl_fd("Error", 2);
-	ft_putendl_fd(msg, 2);
+	if (msg)
+		ft_putendl_fd(msg, 2);
 	exit(1);
 }
