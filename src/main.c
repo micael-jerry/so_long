59ee3534b2904e589431d450acbcfc274d51f31c
FIDIMALALA Micael Jerry <fidimalala.mj@gmail.com>
2025-01-26 09:43:21 +0300

    refactor: rename struct

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 15:23:55 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/26 09:22:32 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/26 09:43:04 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ int	main(int argc, char const *argv[])
 	map_data_validator(map_data);
 	map_content_validator(map_data);
 
-	t_data img;
+	t_data data;
 	void	*mlx;
 	void	*mlx_win;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Game");
-	img.img = mlx_new_image(mlx, 1920,1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	data.img = mlx_new_image(mlx, 1920,1080);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
 
-	put_pixel(&img, 200, 200, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	put_pixel(&data, 200, 200, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, data.img, 0, 0);
 
 	mlx_loop(mlx);
 
