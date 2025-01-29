5d3ee9585c81dab9ae9b0b7c0e34eaa1a01a3006
Micael Jerry <fidimalala.mj@gmail.com>
2025-01-29 06:44:41 +0300

    fix: move on exit

--- src/views/move.c
+++ src/views/move.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 16:28:28 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/28 21:04:20 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/29 06:44:23 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,27 +35,37 @@ int	is_valid_move(t_map_coordinate new_p_coord, t_data *data)
 		return (1);
     if (data->map_data->data[new_p_coord.y][new_p_coord.x] == 'C')
 		return (1);
+    if (data->map_data->data[new_p_coord.y][new_p_coord.x] == 'E')
+		return (1);
+    if (data->map_data->data[new_p_coord.y][new_p_coord.x] == 'T')
+		return (1);
     return (0);
+}
+
+void	set_map_onemove(t_map_coordinate old, t_map_coordinate new, char old_val, char new_val, t_data *data)
+{
+	data->map_data->data[new.y][new.x] = new_val;
+    data->map_data->data[old.y][old.x] = old_val;
 }
 
 void move_player(t_action_key_code action, t_map_coordinate p_coord, t_data *data)
 {
+	char tmp_p_old_val;
 	t_map_coordinate next_p_coord;
 
+	tmp_p_old_val = data->map_data->data[p_coord.y][p_coord.x];
 	next_p_coord = find_next_coordinate(action, p_coord);
 
 	if(is_valid_move(next_p_coord, data))
 	{
 		if (data->map_data->data[next_p_coord.y][next_p_coord.x] == '0')
-		{
-			data->map_data->data[next_p_coord.y][next_p_coord.x] = 'P';
-            data->map_data->data[p_coord.y][p_coord.x] = '0';
-		}
+			set_map_onemove(p_coord, next_p_coord, '0', 'P', data);
 		else if (data->map_data->data[next_p_coord.y][next_p_coord.x] == 'C')
-		{
-			data->map_data->data[next_p_coord.y][next_p_coord.x] = 'P';
-            data->map_data->data[p_coord.y][p_coord.x] = '0';
-		}
+			set_map_onemove(p_coord, next_p_coord, '0', 'P', data);
+		else if (data->map_data->data[next_p_coord.y][next_p_coord.x] == 'E')
+			set_map_onemove(p_coord, next_p_coord, '0', 'T', data);
+		if (tmp_p_old_val == 'T')
+			set_map_onemove(p_coord, next_p_coord, 'E', 'P', data);
 		data->player_move++;
 		ft_printf("Move: %d\n", data->player_move);
 	}
@@ -68,6 +78,8 @@ int	action(t_action_key_code action, t_data *data)
 	if (action == QUIT)
 		return (exit_game(data) ,0);
 	player_coord = find_coordinate_obj(data->map_data->data, data->map_data->heigth, data->map_data->width, 'P');
+	if (player_coord.x == -1 && player_coord.y == -1)
+		player_coord = find_coordinate_obj(data->map_data->data, data->map_data->heigth, data->map_data->width, 'T');
 	move_player(action, player_coord, data);
 	game_render(data);
 	return (0);
