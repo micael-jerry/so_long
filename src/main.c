2618199a443b6aad49f74e143813af2add8f2d12
Micael Jerry <fidimalala.mj@gmail.com>
2025-01-29 09:24:05 +0300

    chore: new exit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 15:23:55 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/29 09:03:45 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/29 09:23:11 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static void	init(t_data *data)
 	data->wall = mlx_xpm_file_to_image(data->mlx, "assets/wall.xpm", &w, &h);
 	data->player = mlx_xpm_file_to_image(data->mlx, "assets/player.xpm", &w,
 			&h);
-	data->exit = mlx_xpm_file_to_image(data->mlx, "assets/house.xpm", &w, &h);
+	data->exit = mlx_xpm_file_to_image(data->mlx, "assets/exit.xpm", &w, &h);
 	data->road = mlx_xpm_file_to_image(data->mlx, "assets/path.xpm", &w, &h);
 	data->player_move = 0;
 	if (!data->collectible_obj || !data->wall || !data->player || !data->road
